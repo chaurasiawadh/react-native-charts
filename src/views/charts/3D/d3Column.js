19c907183023c5dd3fa37fafd82fbfdd51d52818
chaurasiagrv <chaurasiagrv@gmail.com>
2022-09-18 23:26:47 +0530

    removed unwanted code

--- src/views/charts/3D/d3Column.js
+++ src/views/charts/3D/d3Column.js
@@ -63,6 +63,19 @@ export const D3Column = () => {
           height: 90vh;
         },
         </style>
+
+        <style>
+        * {
+          margin: 0;
+          padding: 0;
+          font-family: sans-serif;
+          box-sizing: border-box;
+        }
+        #container {
+          width: 100%;
+          height: 90vh;
+        },
+        </style>
       
         
     <script>
